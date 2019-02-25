7d58602b4466b5332ff45cb403d5ffc258854efc
thaherster <thaherster@gmail.com>
2019-02-25 16:34:42 +0530

    adsas

---
+++ config/keys_dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dbConfig: {
+    host: "127.0.0.1",
+    port: "8529",
+    username: "root",
+    password: "",
+    database: "nodeWebApp"
+  },
+
+  secretOrKey: "secretkey"
+};
